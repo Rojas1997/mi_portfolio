a4994237924534bbeeb271237196f633621177b0
rojas1997 <jose_rojas1997@hotmail.com>
2024-05-03 13:14:03 -0300

    2.0

--- js/java.js
+++ js/java.js
@@ -47,21 +47,3 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
-const optionMenus = document.querySelectorAll(".select-menu"); // Seleccionar todos los menús
-
-optionMenus.forEach(optionMenu => {
-    const selectBtn = optionMenu.querySelector(".select-btn");
-    const options = optionMenu.querySelectorAll(".option");
-    const sBtn_text = optionMenu.querySelector(".Sbtn_text"); // Corregir capitalización
-
-    selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));       
-
-    options.forEach(option =>{
-        option.addEventListener("click", ()=>{
-            let selectedOption = option.querySelector(".option-text").innerText;
-            sBtn_text.innerText = selectedOption;
-
-            optionMenu.classList.remove("active");
-        });
-    });
-});
