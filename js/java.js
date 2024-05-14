ed5e3796b016b9df5d7772a4d275c8eda57d2aa8
Agustin Rojas <131412631+Rojas1997@users.noreply.github.com>
2024-05-14 19:17:10 -0300

    1/54 Grana ctulizacion completa de ioconos y funciones actualizadas

--- js/java.js
+++ js/java.js
@@ -5,33 +5,33 @@ var languageMenu = document.querySelector('.lenguage-menu'); // Selector del men
 
 // Agregamos un listener al botón para alternar la visibilidad del menú
 btnImg.addEventListener('click', function() {
-    toggleMenu();
+toggleMenu();
 });
 
 for (var i = 0; i < langItems.length; i++) {
-    var langItem = langItems[i];
-    langItem.addEventListener('click', function () {
-        changeLanguage(this);
-        toggleMenu(); // Llamamos a la función para alternar la visibilidad del menú después de cambiar el idioma
-    });
+var langItem = langItems[i];
+langItem.addEventListener('click', function () {
+    changeLanguage(this);
+    toggleMenu(); // Llamamos a la función para alternar la visibilidad del menú después de cambiar el idioma
+});
 }
 
 // Función para cambiar el idioma
 function changeLanguage(selectedItem) {
-    btnImg.src = "./img/icon-Idioma/" + selectedItem.dataset.lang + ".png";
-    btnTitle.innerText = selectedItem.dataset.lang;
+btnImg.src = "./img/icon-Idioma/" + selectedItem.dataset.lang + ".png";
+btnTitle.innerText = selectedItem.dataset.lang;
 }
 function changeLanguage(selectedItem) {
-    btnImg.src = "../img/icon-Idioma/" + selectedItem.dataset.lang + ".png";
-    btnTitle.innerText = selectedItem.dataset.lang;
+btnImg.src = "../img/icon-Idioma/" + selectedItem.dataset.lang + ".png";
+btnTitle.innerText = selectedItem.dataset.lang;
 }
 // Función para alternar la visibilidad del menú
 function toggleMenu() {
-    if (languageMenu.style.display === 'none' || languageMenu.style.display === '') {
-        languageMenu.style.display = 'block'; // Mostrar el menú si está oculto
-    } else {
-        languageMenu.style.display = 'none'; // Ocultar el menú si está visible
-    }
+if (languageMenu.style.display === 'none' || languageMenu.style.display === '') {
+    languageMenu.style.display = 'block'; // Mostrar el menú si está oculto
+} else {
+    languageMenu.style.display = 'none'; // Ocultar el menú si está visible
+}
 }
 
 
@@ -42,11 +42,11 @@ function toggleMenu() {
 const optionMenus = document.querySelectorAll(".select-menu"); // Seleccionar todos los menús
 
 optionMenus.forEach(optionMenu => {
-    const selectBtn = optionMenu.querySelector(".select-btn");
-    const options = optionMenu.querySelectorAll(".option");
-    const sBtn_text = optionMenu.querySelector(".Sbtn_text"); // Corregir capitalización
+const selectBtn = optionMenu.querySelector(".select-btn");
+const options = optionMenu.querySelectorAll(".option");
+const sBtn_text = optionMenu.querySelector(".Sbtn_text"); // Corregir capitalización
 
-    selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));       
+selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));       
 
 });
 
@@ -59,73 +59,80 @@ var currentWord = 0;
 // Inicialización de la animación de palabras
 words[currentWord].style.opacity = 1;
 for (var i = 0; i < words.length; i++) {
-    splitLetters(words[i]);
+splitLetters(words[i]);
 }
 
 // Función para cambiar las palabras animadas
 function changeWord() {
-    var cw = wordArray[currentWord];
-    var nw = currentWord == words.length-1 ? wordArray[0] : wordArray[currentWord+1];
-    for (var i = 0; i < cw.length; i++) {
-        animateLetterOut(cw, i);
-    }
-    for (var i = 0; i < nw.length; i++) {
-        nw[i].className = 'letter behind';
-        nw[0].parentElement.style.opacity = 1;
-        animateLetterIn(nw, i);
-    }
+var cw = wordArray[currentWord];
+var nw = currentWord == words.length-1 ? wordArray[0] : wordArray[currentWord+1];
+for (var i = 0; i < cw.length; i++) {
+    animateLetterOut(cw, i);
+}
+for (var i = 0; i < nw.length; i++) {
+    nw[i].className = 'letter behind';
+    nw[0].parentElement.style.opacity = 1;
+    animateLetterIn(nw, i);
+}
 
-    currentWord = (currentWord == wordArray.length-1) ? 0 : currentWord+1;
+currentWord = (currentWord == wordArray.length-1) ? 0 : currentWord+1;
 }
 
 // Funciones de animación de letras
 function animateLetterOut(cw, i) {
-    setTimeout(function() {
-        cw[i].className = 'letter out';
-    }, i*80);
+setTimeout(function() {
+cw[i].className = 'letter out';
+}, i*80);
 }
 
 function animateLetterIn(nw, i) {
-    setTimeout(function() {
-        nw[i].className = 'letter in';
-    }, 340+(i*80));
+setTimeout(function() {
+nw[i].className = 'letter in';
+}, 340+(i*80));
 }
 
 // Función para dividir las palabras en letras
 function splitLetters(word) {
-    var content = word.innerHTML;
-    word.innerHTML = '';
-    var letters = [];
-    for (var i = 0; i < content.length; i++) {
-        var letter = document.createElement('span');
-        letter.className = 'letter';
-        letter.innerHTML = content.charAt(i);
-        word.appendChild(letter);
-        letters.push(letter);
-    }
-
-    wordArray.push(letters);
+var content = word.innerHTML;
+word.innerHTML = '';
+var letters = [];
+for (var i = 0; i < content.length; i++) {
+var letter = document.createElement('span');
+letter.className = 'letter';
+letter.innerHTML = content.charAt(i);
+word.appendChild(letter);
+letters.push(letter);
 }
 
+wordArray.push(letters);
+}
+
+
+/* MENU ESTUDIOS  */
 // Iniciar la animación de palabras y establecer intervalo de cambio
 changeWord();
 setInterval(changeWord, 4000);
 
 // Función para mostrar el submenú de opciones
 function toggleSubMenu(optionHed) {
-    var subMenu = optionHed.nextElementSibling;
-    subMenu.classList.toggle('show');
+var subMenu = optionHed.nextElementSibling;
+subMenu.classList.toggle('show');
 }
 
 // Funciones relacionadas con la visualización de imágenes en un modal
 function showImage(imgUrl) {
-    var modal = document.getElementById('modal'); // Obtener el modal
-    var modalImg = document.getElementById('modal-image'); // Obtener el elemento <img> dentro del modal
-    modalImg.src = imgUrl; // Asignar la URL de la imagen al src del elemento <img> del modal
-    modal.style.display = 'block'; // Mostrar el modal
+var modal = document.getElementById('modal'); // Obtener el modal
+var modalImg = document.getElementById('modal-image'); // Obtener el elemento <img> dentro del modal
+modalImg.src = imgUrl; // Asignar la URL de la imagen al src del elemento <img> del modal
+modal.style.display = 'block'; // Mostrar el modal
 }
 
 function closeModal() {
-    var modal = document.getElementById('modal'); // Obtener el modal
-    modal.style.display = 'none'; // Ocultar el modal cuando se hace clic en cualquier lugar dentro de él
+var modal = document.getElementById('modal'); // Obtener el modal
+modal.style.display = 'none'; // Ocultar el modal cuando se hace clic en cualquier lugar dentro de él
 }
+/* FIN ESTUDIOS  */
+
+
+
+// MODO OSCURO //
